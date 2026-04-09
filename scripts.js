1b79b88d3017c9fdf919abd9d24d92ff882341ec
Otavio de Octavio <Otavio.DOctavio@gmail.com>
2026-04-08 21:25:16 -0300

    mudanças no script

--- scripts.js
+++ scripts.js
@@ -1,32 +1,51 @@
 function calcular(){
-        //1. Obter os valores digitados pelo usuário
-        //number é uma função que converte texto em número
-        // parseInt() converte o texto em número inteiro
-        // || 0 significa "ou zero" (caso o usuário não digite nada, considera 0)
-        // document.getElementById("qtdeArroz") -> busca a caixa de entrada com o id "qtdeArroz"
-        // .value -> pega o valor que está dentro da caixa
-        // JS é uma linguagem não tipada (sem tipo de dados)
-        let arroz = parseInt(document.getElementById("qtdeArroz").value) || 0;
-        let feijao = parseInt(document.getElementById("qtdeFeijao").value) || 0;
-        let Macarrao = parseInt(document.getElementById("qtdeMacarrao").value) || 0;
-        let Soja = parseInt(document.getElementById("qtdeSoja").value) || 0;
-        let Kits = parseInt(document.getElementById("qtdeKits").value) || 0;
-        let Suplementos = parseInt(document.getElementById("qtdeSuplementos").value) || 0;
-        let Leite = parseInt(document.getElementById("qtdeLeite").value) || 0;
+    // JS é uma linguagem não tipada (sem tipo de dados)
+    // Number() é uma função que converte texto em número
 
-        //2. Calcular os pontos
-        // Multiplica a quantidade de arroz por 10 e a quantidade de feijão por 8
+    /* recupera valores de itens avulsos */
 
-        
-        let total = (feijao *8) + (Macarrao*2) + (Soja*4) + (arroz * 10);
-        if (Kits == 60){
-            pontos = pontos + 5000
-        }
-        else{
-            
-        }
+    // recupera o valor informado no elemento com id qtdeArroz
+    let qtdeArroz = Number(document.getElementById("qtdeArroz").value)
+    // recupera o valor informado no elemento com id qtdeFeijao
+    let qtdeFeijao = Number(document.getElementById("qtdeFeijao").value)
+    // recupera o valor informado no elemento com id qtdeOleo
+    let qtdeOleo = Number(document.getElementById("qtdeOleo").value)
+    // recupera o valor informado no elemento com id qtdeMacarrao
+    let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
+    // calcula o valor total de pontos dos itens avulsos
+    let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
 
-
-        //3. Exibir o resultado
-        document.getElementById("resultado").innerText = "Pontuação Total: " + total + " pontos";
+    // calcula o valor total das metas
+    // recupera a cor da equipe
+    let cor = document.getElementById("cor").value
+    let metaProva1 // declara a variável
+    if (cor == "amarela"){
+        metaProva1 = 54
     }
+    else if (cor == "cinza"){
+        metaProva1 = 51
+    }
+    else if (cor == "laranha"){
+        metaProva1 = 21
+    }
+    else if (cor == "marron"){
+        metaProva1 = 88
+    }
+    else if (cor == "preta"){
+        metaProva1 = 60
+    }
+
+    let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value)
+    if (kitAlimentacao >= metaProva1) {
+        pontos = pontos + 5000 // já garantimos 5000
+        if (kitAlimentacao > metaProva1){
+            // ganhamos 83.33 a mais de ponto por itens que ultrapassaram 60
+            pontos = pontos + (kitAlimentacao - metaProva1) * (5000/metaProva1)
+        }
+    }
+    else {
+        pontos = pontos + kitAlimentacao * (5000 / metaProva1)
+    }
+    // vamos exibir o resultado para o usuário
+    document.getElementById("resultado").innerText = "Pontuação total: " + pontos + " pontos"
+}
